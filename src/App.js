648efe44d82d02edfdac05fd89f88d6154253795
Kieter Philip Balisnomo <balisnom@ualberta.ca>
2017-04-01 15:52:51 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="kieter">
+                <div >Hi, I'm&nbsp;
+                    <a href="https://www.instagram.com/kieterb/" target="_blank">Kieter</a>
+                    <div>&nbsp;and I like to&nbsp;
+                        <a href="https://github.com/kieter" target="_blank">code.</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
